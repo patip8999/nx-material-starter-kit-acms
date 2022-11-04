ea2f20a283ba97728dcd37a73db27bcb5f1622b7
patip8999 <patip8999@gmail.comm>
2022-11-04 10:51:39 +0100

    end

--- apps/app/src/app/app-routing.module.ts
+++ apps/app/src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
-import {NgModule} from "@angular/core";
-import {RouterModule} from "@angular/router";
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ProductsComponent} from './components/products/products.component';
+import {ProductFormComponent} from './components/product-form/product-form.component';
+import {DetailsComponent} from './components/details/details.component';
+import {CategoriesComponent} from './components/categories/categories.component';
+import {ProductsComponentModule} from './components/products/products.component-module';
+import {ProductsServiceModule} from './services/products.service-module';
+import {ProductFormComponentModule} from './components/product-form/product-form.component-module';
+import {DetailsComponentModule} from './components/details/details.component-module';
+import {CategoriesComponentModule} from './components/categories/categories.component-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{
+    path: 'products', component: ProductsComponent}, {
+    path: 'add-product', component: ProductFormComponent
+  }, {
+    path: 'product-details/:id', component: DetailsComponent}, {
+    path: 'products/:category', component: CategoriesComponent
+  }]), ProductsComponentModule, ProductsServiceModule, ProductFormComponentModule, DetailsComponentModule, CategoriesComponentModule],
   providers: [],
   declarations: [],
   exports: []
